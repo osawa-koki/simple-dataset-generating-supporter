00502b214c08375944e3ee778406fa78fb5bfe2d
osawa-koki <github@osawa.dev>
2023-03-09 14:42:46 +0900

    例外処理を詳細化した。

--- server/api/post.py
+++ server/api/post.py
@@ -9,21 +9,45 @@ bucket_name = os.environ.get('BUCKET_NAME')
 s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
-    # 受け取ったJSON形式のデータから必要な値を取り出す
-    data = json.loads(event['body'])
-    object_key = data['key']
-    encoded_data = data['image']
+    try:
+        # 受け取ったJSON形式のデータから必要な値を取り出す
+        data = json.loads(event['body'])
+        object_key = data['key']
+        encoded_data = data['image']
+    except Exception as ex:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': 'Invalid request body',
+                'error': 'InvalidRequestError',
+                'detail': str(ex),
+            })
+        }
 
-    # 受け取ったimageプロパティをBASE64デコードする
-    decoded_data = base64.b64decode(encoded_data)
+    try:
+        # 受け取ったimageプロパティをBASE64デコードする
+        decoded_data = base64.b64decode(encoded_data)
+    except Exception as ex:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': 'Invalid image data',
+                'error': 'InvalidImageError',
+                'detail': str(ex),
+            })
+        }
 
     # デコードしたデータが画像データかどうかを確認する
     try:
         image = Image.open(io.BytesIO(decoded_data))
-    except:
+    except Exception as ex:
         return {
             'statusCode': 400,
-            'body': json.dumps({'message': 'Invalid image data', 'error': 'InvalidImageError'})
+            'body': json.dumps({
+                'message': 'Invalid image data',
+                'error': 'InvalidImageError',
+                'detail': str(ex),
+            })
         }
 
     # 画像のサイズが128x128であることを確認する
@@ -34,9 +58,19 @@ def lambda_handler(event, context):
             'body': json.dumps({'message': 'Invalid image size', 'error': 'InvalidImageSizeError'})
         }
 
-    # S3にデータを保存する
-    bucket = s3.Bucket(bucket_name)
-    bucket.put_object(Key=object_key, Body=decoded_data)
+    try:
+        # S3にデータを保存する
+        bucket = s3.Bucket(bucket_name)
+        bucket.put_object(Key=object_key, Body=decoded_data)
+    except Exception as ex:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'message': 'Failed to save data to S3',
+                'error': 'S3Error',
+                'detail': str(ex),
+            })
+        }
 
     return {
         'statusCode': 200,
