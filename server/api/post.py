5a9b0b148b548728d20d2e086f6c414facae4267
osawa-koki <github@osawa.dev>
2023-03-09 10:37:49 +0900

    APIゲートウェイ用の調整。

--- server/api/post.py
+++ server/api/post.py
@@ -1,15 +1,18 @@
 import base64
+import json
 import boto3
 
 s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
+    # 受け取ったJSON形式のデータから必要な値を取り出す
+    data = json.loads(event['body'])
     bucket_name = 'simple-dataset-generating-supporter-image'
-    object_key = event['key']
+    object_key = data['key']
+    encoded_data = data['image']
 
     # 受け取ったimageプロパティをBASE64デコードする
-    encoded_data = event['image']
-    # decoded_data = base64.b64decode(encoded_data)
+    #decoded_data = base64.b64decode(encoded_data)
     decoded_data = encoded_data
 
     # S3にデータを保存する
@@ -18,5 +21,5 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': 'Successfully saved data to S3'
+        'body': json.dumps({'message': 'Successfully saved data to S3'})
     }
