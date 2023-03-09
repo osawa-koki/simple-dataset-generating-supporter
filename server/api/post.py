f3a795c4cc4caeae080378f47068c88f6d19da74
osawa-koki <github@osawa.dev>
2023-03-09 10:10:32 +0900

    S3への保存サンプル。

--- server/api/post.py
+++ server/api/post.py
@@ -1,14 +1,22 @@
-import json
+import base64
+import boto3
 
-def ping(event, context):
-    """意思疎通を確認するためのAPI
-    """
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+    bucket_name = 'simple-dataset-generating-supporter-image'
+    object_key = event['key']
+
+    # 受け取ったimageプロパティをBASE64デコードする
+    encoded_data = event['image']
+    # decoded_data = base64.b64decode(encoded_data)
+    decoded_data = encoded_data
+
+    # S3にデータを保存する
+    bucket = s3.Bucket(bucket_name)
+    bucket.put_object(Key=object_key, Body=decoded_data)
 
     return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world post",
-            }
-        ),
+        'statusCode': 200,
+        'body': 'Successfully saved data to S3'
     }
