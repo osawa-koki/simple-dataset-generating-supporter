fbfcf3444ac11e57738b2196a156eb546b3e645e
osawa-koki <github@osawa.dev>
2023-03-09 10:50:11 +0900

    画像フォーマットとサイズの検証。

--- server/api/post.py
+++ server/api/post.py
@@ -1,5 +1,7 @@
 import base64
+import io
 import json
+from PIL import Image
 import boto3
 import os
 
@@ -13,8 +15,24 @@ def lambda_handler(event, context):
     encoded_data = data['image']
 
     # 受け取ったimageプロパティをBASE64デコードする
-    #decoded_data = base64.b64decode(encoded_data)
-    decoded_data = encoded_data
+    decoded_data = base64.b64decode(encoded_data)
+
+    # デコードしたデータが画像データかどうかを確認する
+    try:
+        image = Image.open(io.BytesIO(decoded_data))
+    except:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'message': 'Invalid image data', 'error': 'InvalidImageError'})
+        }
+
+    # 画像のサイズが128x128であることを確認する
+    width, height = image.size
+    if width != 128 or height != 128:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'message': 'Invalid image size', 'error': 'InvalidImageSizeError'})
+        }
 
     # S3にデータを保存する
     bucket = s3.Bucket(bucket_name)
