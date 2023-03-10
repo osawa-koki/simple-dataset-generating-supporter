045e60574c700cb4c62f34387fb1f4da9ab89247
osawa-koki <github@osawa.dev>
2023-03-10 23:51:38 +0900

    元に戻した。

--- server/api/app.py
+++ server/api/app.py
@@ -303,10 +303,9 @@ def post(event, _):
             'detail': None,
         }),
         'headers': {
-            'Access-Control-Allow-Origin': 'http://localhost:3000',
+            'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent',
             'Access-Control-Allow-Methods': 'GET,OPTIONS,POST,PUT,DELETE',
-            'Access-Control-Allow-Credentials': True,
         },
     }
 
