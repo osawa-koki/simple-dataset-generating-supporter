a1396ef18d3bad7372cc7b1c7a353faff51c3d3b
osawa-koki <github@osawa.dev>
2023-03-10 19:56:56 +0900

    Aboutページ。

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -7,14 +7,10 @@ export default function AboutPage() {
       <div id="About">
         <h1>Here, About page.</h1>
         <p className="mt-3">
-          Next.js大好きです。<br />
-          本当に使いやすいです。<br />
-          僕はTypeScript × Next.jsを強くオススメします。<br />
+          機械学習用のデータセットを自作するためのサポートサイトです。<br />
+          簡単に画像をお絵描きして、データセットを作成できます。<br />
           <br />
-          静的型付け言語って難しいと思われがちですが、エディタの機能が豊富な現在は型付け言語の方が簡単に書くことができます。<br />
-          さらには字句解析と構文解析に加えて、コンパイル時に、コンパイラによる意味解析を行ってくれるため、動的型付け言語と比較して動作に信頼性があります。<br />
-          <br />
-          また、Reactと比べてNext.jsはSSGによってビルド時にHTMLが生成されるため、SPA(SSR)と比べて高速に動作し、ユーザビリティが高いです。
+          このプログラムは「S3」「Lambda」「GitHub Actions」および「Next.js」を使用しています。
         </p>
       </div>
     </Layout>
