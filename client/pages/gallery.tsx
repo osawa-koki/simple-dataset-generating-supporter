f84d83677f676b7bc6c4405b8b490a97d7c7ded2
osawa-koki <github@osawa.dev>
2023-03-11 12:04:26 +0900

    GalleryPage雛形。

---
+++ client/pages/gallery.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function GalleryPage() {
+  return (
+    <Layout>
+      <div id="Gallery">
+        <h1>🖼️ Gallery 🖼️</h1>
+      </div>
+    </Layout>
+  );
+};
